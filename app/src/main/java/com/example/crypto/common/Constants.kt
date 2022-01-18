c02aa659f821d394506c657c11177d9dd4d97791
Yuliia Koba <yuliia.koba@proarea.com>
2022-01-18 18:23:26 +0200

    add generic

---
+++ app/src/main/java/com/example/crypto/common/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.crypto.common
+
+object Constants {
+}
--- app/src/main/java/com/example/crypto/presentation/MainActivity.kt
+++ app/src/main/java/com/example/crypto/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.crypto.presentation.ui
+package com.example.crypto.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
